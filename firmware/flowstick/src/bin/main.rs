80ca96de486313f177073b60cce87b336fb5c649
Eric Van Albert <eric@van.al>
2025-07-30 18:14:56 -0400

    add SPI

--- firmware/flowstick/src/bin/main.rs
+++ firmware/flowstick/src/bin/main.rs
@@ -4,26 +4,20 @@
 use esp_alloc as _;
 use esp_backtrace as _;
 use esp_hal::{
-    dma, dma_circular_buffers, gpio, i2s, main, time, usb_serial_jtag::UsbSerialJtag, Blocking,
+    dma, dma_circular_buffers, gpio, i2s, main, rng, spi, time, timer,
+    usb_serial_jtag::UsbSerialJtag, Blocking,
 };
 //use log::info;
 use esp_println::println;
 
-/*
 use bleps::{
-    Ble,
-    HciConnector,
     ad_structure::{
-        AdStructure,
-        BR_EDR_NOT_SUPPORTED,
-        LE_GENERAL_DISCOVERABLE,
-        create_advertising_data,
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
     },
-    gatt,
     attribute_server::AttributeServer,
+    gatt, Ble, HciConnector,
 };
 use esp_wifi::ble::controller::BleConnector;
-*/
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -41,6 +35,7 @@ pub struct Hardware<'d> {
     vbus_sense: &'d gpio::Input<'static>,
     chg_sense: &'d gpio::Input<'static>,
     i2s_transfer: dma::DmaTransferTxCircular<'d, i2s::master::I2sTx<'static, Blocking>>,
+    spi: spi::master::Spi<'static, Blocking>,
 }
 
 pub fn with_hardware<T, F: FnOnce(Hardware) -> T>(f: F) -> T {
@@ -55,6 +50,14 @@ pub fn with_hardware<T, F: FnOnce(Hardware) -> T>(f: F) -> T {
         gpio::OutputConfig::default(),
     );
 
+    // BATT_MEASURE_EN
+    // This line should be held low always
+    gpio::Output::new(
+        peripherals.GPIO18,
+        gpio::Level::Low,
+        gpio::OutputConfig::default(),
+    );
+
     let button = gpio::Input::new(
         peripherals.GPIO10,
         gpio::InputConfig::default().with_pull(gpio::Pull::Down),
@@ -65,6 +68,7 @@ pub fn with_hardware<T, F: FnOnce(Hardware) -> T>(f: F) -> T {
         gpio::InputConfig::default().with_pull(gpio::Pull::Up),
     );
 
+    // I2S for writing out to the LEDs
     let (_, _, mut i2s_tx_buffer, i2s_tx_descriptors) =
         dma_circular_buffers!(FRAME_SIZE_BYTES * 64); // Chunk size must be dma::CHUNK_SIZE
 
@@ -86,12 +90,104 @@ pub fn with_hardware<T, F: FnOnce(Hardware) -> T>(f: F) -> T {
 
     let i2s_transfer = i2s_tx.write_dma_circular(&mut i2s_tx_buffer).unwrap();
 
+    // SPI for the accelerometer
+    let mut spi = spi::master::Spi::new(
+        peripherals.SPI2,
+        spi::master::Config::default()
+            .with_frequency(time::Rate::from_khz(100))
+            .with_mode(spi::Mode::_0),
+    )
+    .unwrap()
+    .with_miso(peripherals.GPIO33)
+    .with_mosi(peripherals.GPIO34)
+    .with_sck(peripherals.GPIO35)
+    .with_cs(peripherals.GPIO36);
+
+    /*
+    // Bluetooth
+    let timg0 = timer::timg::TimerGroup::new(peripherals.TIMG0);
+    let esp_wifi_ctrl = esp_wifi::init(timg0.timer0, rng::Rng::new(peripherals.RNG)).unwrap();
+    let mut bluetooth = peripherals.BT;
+
+    let now = || time::Instant::now().duration_since_epoch().as_millis();
+    loop {
+        let connector = BleConnector::new(&esp_wifi_ctrl, bluetooth.reborrow());
+        let hci = HciConnector::new(connector, now);
+        let mut ble = Ble::new(&hci);
+
+        println!("{:?}", ble.init());
+        println!("{:?}", ble.cmd_set_le_advertising_parameters());
+        println!(
+            "{:?}",
+            ble.cmd_set_le_advertising_data(
+                create_advertising_data(&[
+                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+                    AdStructure::CompleteLocalName("Flowstick"),
+                ])
+                .unwrap()
+            )
+        );
+        println!("{:?}", ble.cmd_set_le_advertise_enable(true));
+
+        println!("started advertising");
+
+        let mut rf = |_offset: usize, data: &mut [u8]| {
+            data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
+            17
+        };
+        let mut wf = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut wf2 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: {} {:?}", offset, data);
+        };
+
+        let mut rf3 = |_offset: usize, data: &mut [u8]| {
+            data[..5].copy_from_slice(&b"Hola!"[..]);
+            5
+        };
+        let mut wf3 = |offset: usize, data: &[u8]| {
+            println!("RECEIVED: Offset {}, data {:?}", offset, data);
+        };
+
+        gatt!([service {
+            uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+            characteristics: [
+                characteristic {
+                    uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
+                    read: rf,
+                    write: wf,
+                },
+                characteristic {
+                    uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
+                    write: wf2,
+                },
+                characteristic {
+                    name: "my_characteristic",
+                    uuid: "987312e0-2354-11eb-9f10-fbc30a62cf38",
+                    notify: true,
+                    read: rf3,
+                    write: wf3,
+                },
+            ],
+        },]);
+
+        let mut rng = bleps::no_rng::NoRng;
+        let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
+
+        loop {}
+    }
+    */
+
     f(Hardware {
         pwrhld: &mut pwrhld,
         button: &button,
         vbus_sense: &vbus_sense,
         chg_sense: &chg_sense,
         i2s_transfer,
+        spi,
     })
 }
 
@@ -243,89 +339,6 @@ impl PowerState {
 #[main]
 fn main() -> ! {
     with_hardware(|mut hardware| {
-        /*
-
-            // Bluetooth
-            let timg0 = timg::TimerGroup::new(peripherals.TIMG0);
-            let esp_wifi_ctrl = esp_wifi::init(timg0.timer0, rng::Rng::new(peripherals.RNG)).unwrap();
-            let mut bluetooth = peripherals.BT;
-
-            let now = || time::Instant::now().duration_since_epoch().as_millis();
-            loop {
-                let connector = BleConnector::new(&esp_wifi_ctrl, bluetooth.reborrow());
-                let hci = HciConnector::new(connector, now);
-                let mut ble = Ble::new(&hci);
-
-                println!("{:?}", ble.init());
-                println!("{:?}", ble.cmd_set_le_advertising_parameters());
-                println!(
-                    "{:?}",
-                    ble.cmd_set_le_advertising_data(
-                        create_advertising_data(&[
-                            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
-                            AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
-                            AdStructure::CompleteLocalName("Flowstick"),
-                        ])
-                        .unwrap()
-                    )
-                );
-                println!("{:?}", ble.cmd_set_le_advertise_enable(true));
-
-                println!("started advertising");
-
-                let mut rf = |_offset: usize, data: &mut [u8]| {
-                    data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
-                    17
-                };
-                let mut wf = |offset: usize, data: &[u8]| {
-                    println!("RECEIVED: {} {:?}", offset, data);
-                };
-
-                let mut wf2 = |offset: usize, data: &[u8]| {
-                    println!("RECEIVED: {} {:?}", offset, data);
-                };
-
-                let mut rf3 = |_offset: usize, data: &mut [u8]| {
-                    data[..5].copy_from_slice(&b"Hola!"[..]);
-                    5
-                };
-                let mut wf3 = |offset: usize, data: &[u8]| {
-                    println!("RECEIVED: Offset {}, data {:?}", offset, data);
-                };
-
-                gatt!([service {
-                    uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
-                    characteristics: [
-                        characteristic {
-                            uuid: "937312e0-2354-11eb-9f10-fbc30a62cf38",
-                            read: rf,
-                            write: wf,
-                        },
-                        characteristic {
-                            uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
-                            write: wf2,
-                        },
-                        characteristic {
-                            name: "my_characteristic",
-                            uuid: "987312e0-2354-11eb-9f10-fbc30a62cf38",
-                            notify: true,
-                            read: rf3,
-                            write: wf3,
-                        },
-                    ],
-                },]);
-
-                let mut rng = bleps::no_rng::NoRng;
-                let mut srv = AttributeServer::new(
-                    &mut ble,
-                    &mut gatt_attributes,
-                    &mut rng,
-                );
-
-                loop {}
-            }
-        */
-
         let mut power_state = PowerState::Off;
         let mut anim = Anim::Off;
         let mut last_button_pressed = false;
